d89cae6d8c2ac5230b6eb839aa8cb78c3edd18b1
Garrett <gjblythe@gmail.com>
2019-01-15 09:34:35 -0700

    without userAuth

---
+++ src/GitReset.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { TouchableOpacity, Text, Button, StyleSheet, View } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import AntiClippyL from './AntiClippyL'
+import * as Animatable from 'react-native-animatable'
+//route to push
+const GitReset = () => {
+   const goToPush = () => {
+      Actions.gitPush()
+   }
+   return (
+      <View style={styles.container}>
+      <View style={styles.brownBox}>
+      <Animatable.Text animation="zoomInDown"><Text style={styles.title}>Git Reset</Text></Animatable.Text>
+      <View style={styles.circle1}>
+      <AntiClippyL style={styles.AntiClippy}/>
+      <Text style={styles.p}>Oh S#!% button!! Undoes all commits, preserving changes locally. Start over if needed.. Type <Text style={{ fontWeight: 'bold' }}>git reset</Text> to discard last commit. *Note: you MUST do this before you push!!</Text>
+   
+      
+     
+      </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+         <Button 
+         onPress = {goToPush}
+         title= "GitGoing to Git Push"
+         color='#FF6D70'
+         />
+      </TouchableOpacity>
+      </View>
+         
+         
+   )
+}
+//style
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#4A4843',
+      alignItems: "center",      
+    },
+    
+   title: {
+      color: '#FF6D70',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    fontSize: 50,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+    marginTop: 40,
+    paddingTop: 175,
+    
+   },
+   //white circle
+  circle1: {
+   backgroundColor: '#fff',
+   width: 400,
+   height: 400,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 200,
+   borderColor: '#4A4843',
+   borderWidth: 15,
+   marginTop:60,
+   flexDirection: 'row',
+   alignItems: 'center',
+   
+ },
+ //brown box
+ brownBox: {
+   backgroundColor: '#A79B82',
+   width: 410,
+   height:575,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 220,
+   marginTop: 30,
+   paddingBottom: 25,
+ },
+
+//logo
+ AntiClippy: {
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+
+ 
+ button: {
+    color:'#FF6D70',
+    marginTop: 20,
+ },
+ p:{
+    color: 'black',
+    width: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: 18,
+
+ },
+
+
+ });
+export default GitReset
---
+++ src/Home.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import { TouchableOpacity, Text, Button, StyleSheet, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { TextInput } from "react-native";
+import AntiClippyHalf from './AntiClippyHalf';
+import * as Animatable from 'react-native-animatable'
+
+// init.startApp();
+
+// App.initializeApp();
+
+const goToGitInit = () => {
+   Actions.gitInit()
+}
+
+export default class InputBox extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = ({
+      email: '',
+      password: ''
+    })
+  }
+  
+render(){
+   return (
+    
+      <View style={styles.container}>
+      <View style={styles.textBox1}>
+      <View style={styles.circle}>
+      <Animatable.View animation="fadeInDown"><AntiClippyHalf style={styles.AntiClippyHalf}/></Animatable.View>
+      <Animatable.Text animation="rubberBand"><Text style={styles.title}>GitGoing</Text></Animatable.Text>
+      </View>
+      
+      </View> 
+      <TouchableOpacity style = {{ marginTop: 250 }}> 
+    
+         <Button 
+         onPress = {goToGitInit}
+         title= "GitGoing to Git Init"
+         color='#FF6D70'
+         />  
+      </TouchableOpacity>
+      <Text style={styles.p}>Would you like to learn more?</Text>
+      </View>
+     
+   )
+}
+}
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#4A4843',
+      alignItems: "center",      
+    },
+    
+   title: {
+      color: '#FF6D70',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 40,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+    marginTop: 0,
+    marginBottom: 0,
+   },
+   //white circle
+  circle: {
+   backgroundColor: '#fff',
+   width: 400,
+   height: 400,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 200,
+   borderColor: '#4A4843',
+   borderWidth: 25,
+   marginTop:220,
+   
+ },
+ //brown box
+ textBox1: {
+   backgroundColor: '#A79B82',
+   width: 500,
+   height: 350,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 30,
+   marginTop: 50,
+ },
+
+//logo
+ AntiClippyHalf: {
+   flex: 1
+ },
+
+ //username
+ textInput1: {
+   color: 'black',
+   borderWidth: 2,
+   borderColor: 'black',
+   backgroundColor: '#FFF',
+   borderRadius: 20,
+   width: 200,
+   marginTop: 0,
+   alignItems: 'center',   
+   
+ },
+
+ //password
+ textInput: {
+   color: 'black',
+   borderWidth: 2,
+   borderColor: 'black',
+   backgroundColor: '#FFF',
+   borderRadius: 20,
+   width: 200,
+   marginTop: 10,
+   marginBottom: 20,
+   alignItems: 'center',
+ },
+
+ p:{
+    color: 'white',
+ },
+
+
+ });
